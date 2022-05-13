49e9573fd96ce85ebb50a4112933d059378af802
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-05-13 14:08:01 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,35 @@
-import express from "express"
-import productsRouter from "./apis/products/index.js"
-import cors from "cors"
-import listEndpoints from "express-list-endpoints"
+import express from "express";
+import productsRouter from "./apis/products/index.js";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 import {
   handleBadRequest,
   handleNotFound,
   handleUnauthorized,
   handleServer,
-} from "./errorHandlers.js"
+} from "./errorHandlers.js";
+import { join } from "path";
 
-const PORT = 3001
-const server = express()
-server.use(express.json())
-server.use(cors())
+const server = express();
 
-server.use("/products", productsRouter)
+const publicFolderPath = join(process.cwd(), "./public");
+console.log(publicFolderPath);
+
+const PORT = 3001;
+server.use(express.json());
+server.use(express.static(publicFolderPath));
+
+server.use(cors());
+
+server.use("/products", productsRouter);
 
 // error handlers
-server.use(handleBadRequest)
-server.use(handleNotFound)
-server.use(handleUnauthorized)
-server.use(handleServer)
+server.use(handleBadRequest);
+server.use(handleNotFound);
+server.use(handleUnauthorized);
+server.use(handleServer);
 
 server.listen(PORT, () => {
-  console.table(listEndpoints(server))
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.table(listEndpoints(server));
+  console.log(`Server is running on port ${PORT}`);
+});
