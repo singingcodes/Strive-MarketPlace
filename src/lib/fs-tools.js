75a11d939568ab00748714c779482408e46ad92f
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-06-05 14:03:47 +0200

    Update fs-tools.js

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,22 +1,22 @@
-import fs from "fs-extra"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
-const { readJSON, writeJSON, writeFile } = fs
+import fs from "fs-extra";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const { readJSON, writeJSON, writeFile } = fs;
 
-const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
-const productsJSONPath = join(dataFolderPath, "products.json")
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
+const productsJSONPath = join(dataFolderPath, "products.json");
 
-const productsPublicFolderPath = join(process.cwd(), "./public/img/products")
+const productsPublicFolderPath = join(process.cwd(), "./public/img/products");
 
-export const getProducts = () => readJSON(productsJSONPath)
+export const getProducts = () => readJSON(productsJSONPath);
 export const writeProducts = (productsArray) =>
-  writeJSON(productsJSONPath, productsArray)
+  writeJSON(productsJSONPath, productsArray);
 
 export const saveProductsImageUrl = (fileName, contentAsBuffer) => {
-  const filePath = join(productsPublicFolderPath, fileName)
-  const savedPath = `/img/products/${fileName}`
-  console.log(savedPath)
-  writeFile(filePath, contentAsBuffer)
-  const url = `http://localhost:3001${savedPath}`
-  return url
-}
+  const filePath = join(productsPublicFolderPath, fileName);
+  const savedPath = `/img/products/${fileName}`;
+  console.log(savedPath);
+  writeFile(filePath, contentAsBuffer);
+  const url = `http://localhost:3001${savedPath}`;
+  return url;
+};
